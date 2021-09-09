702139e7817cd3fd54a0e53ff20a856c902cb3e5
sanju6890 <sanjusaikap2001@gmail.com>
2021-09-09 21:21:40 +0530

    Modified settings.py to upload images
    Signed-off-by: sanju6890 <sanjusaikap2001@gmail.com>

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
