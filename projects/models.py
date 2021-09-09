12160c8c07d1bbc23fc140736541af7d9e5ec6cc
sanju6890 <sanjusaikap2001@gmail.com>
2021-09-09 15:15:49 +0530

    Modified upload image property
    Signed-off-by: sanju6890 <sanjusaikap2001@gmail.com>

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField()
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=200)
     url = models.CharField(max_length=500)
