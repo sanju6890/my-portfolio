514512cab70c9caf8f8717330b8f05027316e365
sanju6890 <sanjusaikap2001@gmail.com>
2022-02-26 16:06:58 +0530

    Updated versions of dependencies in the requirements.txt file
    Signed-off-by: sanju6890 <sanjusaikap2001@gmail.com>

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,7 +19,6 @@ import projects.views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
